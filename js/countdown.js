6f18bf040a0c9a74d979be59ecb7ed7a7320f749
PepeDeChuy15 <peperety15@gmail.com>
2026-02-22 14:27:43 -0600

    Se agrega JS

---
+++ js/countdown.js
@@ -0,0 +1,23 @@
+const weddingDate = new Date("October 24, 2026 15:00:00").getTime();
+
+const timer = setInterval(function() {
+
+    const now = new Date().getTime();
+    const distance = weddingDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerText = days;
+    document.getElementById("hours").innerText = hours;
+    document.getElementById("minutes").innerText = minutes;
+    document.getElementById("seconds").innerText = seconds;
+
+    if (distance < 0) {
+        clearInterval(timer);
+        document.querySelector(".countdown").innerHTML = "<h2>We are married! ❤️</h2>";
+    }
+
+}, 1000);
